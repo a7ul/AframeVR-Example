f2a242ead90580b20492a4e43d388e48acfa57fa
Atul <atulanand94@gmail.com>
2016-06-03 12:47:01 +0530

    Added movement;

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import AFRAME from 'aframe';
-AFRAME.registerComponent('obj-loader', require('aframe-obj-loader-component'));
 var extras = require('aframe-extras');
 extras.registerAll();
+
 window.onload= function(){
   require('./vr-main.js');
 };
--- src/vr-main.js
+++ src/vr-main.js
@@ -1,15 +1,29 @@
 import $ from 'jquery';
 
-let markerListener = (label,camera)=>{
-  $('#marker-'+label).click(()=>{
-    $('.camera').attr('active',false);
-    $('#camera-'+camera).attr('active',true);
-  });
-};
+let camera = $('#camera');
+window.camera = camera;
+let arrow = $('#arrow');
+let flag = true;
+let keyflag = true;
+let keyCode = 'W'.charCodeAt(0);
 
-markerListener('mainpathway','mainpathway');
-markerListener('frontdesk','frontdesk');
-markerListener('kitchen','kitchen');
-markerListener('mainpathway2','mainpathway');
-markerListener('mainpathway3','mainpathway');
-markerListener('silentzone','silentzone');
+setInterval(() => {
+    flag = !flag;
+}, 15);
+setInterval(() => {
+    keyflag = !keyflag;
+}, 100);
+
+camera[0].addEventListener('componentchanged', function(evt) {
+    if (!flag) {
+        if (evt.detail.name = 'position') {
+            let {x, y, z} = evt.detail.newData;
+            arrow.attr('position', x + ' ' + 0 + ' ' + z);
+        }
+        if (arrow[0].is('cursor-hovered') && keyflag) {
+            camera[0].components['wasd-controls'].keys[keyCode] = true;
+        } else {
+            camera[0].components['wasd-controls'].keys[keyCode] = false;
+        };
+    }
+});
